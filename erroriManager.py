f51238cd8904ebb7134447d6ffecc1d4940cc4b7
ildive <60446683+ildive@users.noreply.github.com>
2023-11-03 21:20:05 +0100

    Update erroriManager.py

--- erroriManager.py
+++ erroriManager.py
@@ -1,6 +1,6 @@
 import telebot
 
-bot = telebot.TeleBot("6694548358:AAEyP9jRmRLl65dhVGJk_tXAsScUIJr00Vk", parse_mode="HTML")
+bot = telebot.TeleBot("<TOKEN>", parse_mode="HTML")
 
 def sendErrorModuloNonTrovato(chat_id):
     bot.send_message(chat_id, "C'è stato un'errore. Contatta l'assistenza cliccando qui sotto")
